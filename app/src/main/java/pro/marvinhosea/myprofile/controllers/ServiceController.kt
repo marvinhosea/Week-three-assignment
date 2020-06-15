96f96850971ce64931390169b356e4911247b66f
kodaTheCoder <marvincollins14@gmail.com>
2020-06-15 18:58:45 +0300

    Completed designing service controller

--- app/src/main/java/pro/marvinhosea/myprofile/controllers/ServiceController.kt
+++ app/src/main/java/pro/marvinhosea/myprofile/controllers/ServiceController.kt
@@ -3,5 +3,19 @@ package pro.marvinhosea.myprofile.controllers
 import pro.marvinhosea.myprofile.models.Service
 
 class ServiceController{
-    val services: MutableList<Service> = MutableList()
+    val arrayOfServices: Array<Service> = arrayOf(
+        Service("Painting", 600, "We paint houses, shops, vehicles and containers. There is art in our painting"),
+        Service("Animal House Construction", 30000, "We are professional animal house constructors with twenty years of experience"),
+        Service("Bookkeeping", 340000, "Are you struggling with bookkeeping? Then worry no more we're a team of accountants dedicated to keeping your books clean")
+    )
+    val services: MutableList<Service> = arrayOfServices.toMutableList()
+
+    fun getSingleService(): Service {
+        return services.random()
+    }
+
+    fun getAllServices(): MutableList<Service> {
+        return services
+    }
+
 }
