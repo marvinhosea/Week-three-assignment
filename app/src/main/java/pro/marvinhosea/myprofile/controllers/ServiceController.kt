03e62115504cc6791522b90d76cbc5cc790703a8
kodaTheCoder <marvincollins14@gmail.com>
2020-06-15 18:40:53 +0300

    Added ServiceController.kt

---
+++ app/src/main/java/pro/marvinhosea/myprofile/controllers/ServiceController.kt
@@ -0,0 +1,7 @@
+package pro.marvinhosea.myprofile.controllers
+
+import pro.marvinhosea.myprofile.models.Service
+
+class ServiceController{
+    val services: MutableList<Service> = MutableList()
+}
